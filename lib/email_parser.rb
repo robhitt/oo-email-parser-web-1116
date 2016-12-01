f8166440a30c3ff1cb0f6e035999888c80027422
Rob Hitt <robhitt@gmail.com>
2016-11-30 21:04:32 -0500

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,28 @@
-# Build a class EmailParser that accepts a string of unformatted 
+# Build a class EmailParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailParser
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    # "rob@hitt.com, go@hello.com, clinton@n.com"
+    email_array = @emails.split(" ")
+
+    email_array.map do |indv_address|
+      indv_address.gsub(/,/,"")
+    end.uniq
+    #remove_white_space(email_array)
+  end
+
+  def remove_white_space(email_array)
+    email_array.map { |email_address| email_address.strip }
+  end
+end
+
+# test_class = EmailParser.new("rob@hitt.com, go@hello.com, clinton@n.com")
+# print test_class.parse
